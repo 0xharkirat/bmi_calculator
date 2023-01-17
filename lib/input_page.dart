14e2f778912c6c26866a82c6cb381e557f3722d3
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-01-17 20:17:01 +1100

    Slider theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -89,19 +89,26 @@ class _InputPageState extends State<InputPage> {
                       )
                     ],
                   ),
-                  Slider(
-                      value: height.toDouble(),
-                      min: 120,
-                      max: 220,
-                      activeColor: Color(0xFFEB1555),
-                      inactiveColor: Color(0xFF8D8E98),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                        value: height.toDouble(),
+                        min: 120,
+                        max: 220,
+                        onChanged: (double newValue){
+                          setState(() {
+                            height = newValue.round();
+                          });
 
-                      onChanged: (double newValue){
-                        setState(() {
-                          height = newValue.round();
-                        });
-
-                      }
+                        }
+                    ),
                   ),
                 ],
               ),
